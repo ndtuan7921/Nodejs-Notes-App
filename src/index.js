8a85c4c3e39480ac95b4caa75bd1921b8c5b6272
ndtuan7921 <ndtuan7921@gmail.com>
2023-03-08 17:56:45 +0700

    get started with node, packages, init config

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const handlebars = require("express-handlebars");
+const morgan = require("morgan");
+
+const app = express();
+const port = 7921;
+
+// HTTP logger
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  ".hbs",
+  handlebars.engine({
+    extname: ".hbs",
+    // defaultLayout: "base",
+    layoutsDir: path.join(__dirname, "./views/layouts"),
+    partialsDir: [
+      //  path to your partials
+      path.join(__dirname, "./views/partials"),
+    ],
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
