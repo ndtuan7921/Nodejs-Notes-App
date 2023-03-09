42f743c429cdc3a8d30a78e36c5b44667647d466
ndtuan7921 <ndtuan7921@gmail.com>
2023-03-09 16:23:30 +0700

    config sass and style home page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const morgan = require("morgan");
 const app = express();
 const port = 7921;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 app.use(morgan("combined"));
 
