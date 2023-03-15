0080e13fd2a995d1f55a941eed5287abb8d8179f
ndtuan7921 <ndtuan7921@gmail.com>
2023-03-16 01:25:56 +0700

    handle search function

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,8 +5,23 @@ class DashboardController {
   index(req, res) {
     db.query("SELECT * FROM NOTES_APP.NOTE", (err, result) => {
       if (err) throw err;
+      // console.log(result);
       res.render("../views/dashboard/dashboard.hbs", { result });
     });
+  }
+
+  // POST /dashboard/search
+  search(req, res) {
+    // console.log(req);
+    const query = req.body.searchTerm;
+    db.query(
+      `SELECT * FROM NOTES_APP.NOTE WHERE title LIKE '%${query}%'`,
+      (err, result) => {
+        if (err) throw err;
+        // console.log(result);
+        res.render("../views/dashboard/dashboard.hbs", { result });
+      }
+    );
   }
 
   // GET /dashboard/add
--- src/routes/dashboard.route.js
+++ src/routes/dashboard.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const dashboard = require("../controllers/dashboard.controller");
 
 router.get("/add", dashboard.create);
-
+router.post("/search", dashboard.search);
 router.get("/", dashboard.index);
 
 module.exports = router;
